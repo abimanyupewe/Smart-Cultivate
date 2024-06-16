0dbeb5602b85db967acc58b755aaaef579afc0fd
abimanyupewe <abimanyupw369@gmail.com>
2024-06-16 20:20:09 +0700

    Baru sir

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -8,13 +8,32 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Get.offAll(HomePage());
-          },
-          child: Text("Login"),
-        ),
+      body: ListView(
+        children: [
+          Center(
+            child: Image(
+              image: AssetImage("assets/images/Gardening-cuate.png"),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Get.offAll(HomePage());
+            },
+            child: Container(
+              width: 350, // lebar tombol
+              height: 50, // tinggi tombol
+              decoration: BoxDecoration(
+                color: Color(0xFF74512D),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                'Sign',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
